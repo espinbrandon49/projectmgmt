fc8db95cb54a0fe0b0004396077cd5e5d7940321
Brandon Espinosa <espinbrandon49@gmail.com>
2023-01-28 17:44:07 -0800

    add update projects

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,8 +1,10 @@
 import { Link, useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
-import { useQuery } from '@apollo/client';
+import { useQuery} from '@apollo/client';
 import { GET_PROJECT } from '../queries/projectQueries';
 import ClientInfo from '../components/ClientInfo';
+import DeleteProjectButton from '../components/DeleteProjectButton';
+import EditProjectForm from '../components/EditProjectForm';
 
 export default function Project() {
   const { id } = useParams();
@@ -25,6 +27,11 @@ export default function Project() {
           <p className='lead'>{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <EditProjectForm project={data.project} />
+
+          <DeleteProjectButton projectId={data.project.id} />
+
         </div>
       )}
     </>
